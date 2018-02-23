a25ee8a70e2f604b80516cac41b103e98479741f
SeydX <34152761+SeydX@users.noreply.github.com>
2018-02-24 00:26:23 +0100

    Update Requests.js

--- src/Requests.js
+++ src/Requests.js
@@ -104,6 +104,35 @@ class TADO_REQ {
 
     }
 
+    HOME_DEVICES() {
+
+        var self = this;
+
+        this.tado = {
+
+            token: null,
+
+            get: function() {
+
+                return rp({
+
+                    "method": "GET",
+                    "uri": "https://my.tado.com/api/v2/homes/" + self.homeID + "/zones/" + self.zoneID + "/devices",
+                    "qs": {
+                        "password": self.password,
+                        "username": self.username
+                    },
+                    "json": true
+
+                });
+            }
+        }
+
+        self.tado.token = self.params.token;
+        return self.tado.get();
+
+    }
+
     STATE() {
 
         var self = this;
